fd0d01941342631f152a579bed486bed25c3e74a
Antoine Niek <antoine.niek@adgear.com>
2017-02-07 10:23:42 -0500

    Add exit builtin

--- src/builtins.rs
+++ src/builtins.rs
@@ -3,6 +3,7 @@ extern crate nix;
 use std::env::home_dir;
 use nix::unistd;
 use std::io::{self, Write};
+use std::process;
 
 pub fn cd(args: &[&str]) -> nix::Result<()> {
     let path = match args.get(1) {
@@ -17,4 +18,13 @@ pub fn echo(args: &[&str]) -> nix::Result<()> {
     println!("{}", args[1..].join(" "));
     io::stdout().flush().unwrap();
     Ok(())
+}
+
+pub fn exit(args: &[&str]) -> nix::Result<()> {
+    let status: i32 = args.get(0)
+        .unwrap_or(&"0")
+        .parse::<i32>()
+        .unwrap_or(0);
+
+    process::exit(status);
 }
